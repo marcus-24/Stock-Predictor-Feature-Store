af01750de957d50b77ccc48fcfedb2671e764cf8
mca36 <89984970+marcus-24@users.noreply.github.com>
2025-03-14 17:59:53 -0400

    Working hopsworks environment. Added cron job

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # Stock-Predictor-Feature-Store
 
 This repository uses [Hopsworks](https://www.hopsworks.ai/) as a Feature Store to serve features to the Stock Predictor training pipeline and inference services to keep the feature transformations consistent and reduce redundant feature calculations.
+
+# References
+
+- [Hopsworks Tutorial Github](https://github.com/logicalclocks/hopsworks-tutorials/tree/master)
--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import hopsworks
-from hsfs.feature_store import FeatureStore
 from hsfs.feature_group import FeatureGroup
+from hsfs.feature_store import FeatureStore
 import yfinance as yf
 from datetime import date
 from dateutil.relativedelta import relativedelta
@@ -15,8 +15,18 @@ API_KEY = os.getenv("HOPSWORKS_KEY")
 
 
 def feature_engineering(df: pd.DataFrame, win_size: int = WIN_SIZE) -> pd.DataFrame:
+    """Generates window features for the stock prediction
+
+    Args:
+        df (pd.DataFrame): raw stock prediction data from yahoo finance
+        win_size (int, optional): Look back window for features. Defaults to WIN_SIZE.
+
+    Returns:
+        pd.DataFrame: features for stock prediction model and ready for hopsworks feature store ingestion
+    """
     # https://medium.com/aimonks/improving-stock-price-forecasting-by-feature-engineering-8a5d0be2be96
     _df = df.copy()
+    # TODO: Labels use a negative shift in for loop
 
     return (
         _df.assign(
@@ -40,15 +50,23 @@ def feature_engineering(df: pd.DataFrame, win_size: int = WIN_SIZE) -> pd.DataFr
 
 
 def create_labels(df: pd.DataFrame, n_future: int = N_FUTURE) -> pd.DataFrame:
+    """Generates labels for model training
+
+    Args:
+        df (pd.DataFrame): raw stock prediction data from yahoo finance
+        n_future (int, optional): The number of time steps ahead of time the model needs to predict. Defaults to N_FUTURE.
+
+    Returns:
+        pd.DataFrame: future time steps that the model needs to predict
+    """
     _df = df.copy()
     for idx in range(n_future):
-        _df[f"label_{idx + 1}"] = _df["Close"].shift(idx + 1)
+        _df[f"label_time_{idx + 1}"] = _df["Close"].shift(-idx - 1)
 
     return _df.dropna().drop(columns=df.columns).reset_index().rename(columns=str.lower)
 
 
-if __name__ == "__main__":
-
+def main() -> None:
     start_date = date.today() - relativedelta(years=2)
     df = yf.Ticker("AAPL").history(interval="1d", start=start_date)
     feature_df = feature_engineering(df)
@@ -56,7 +74,11 @@ if __name__ == "__main__":
 
     project = hopsworks.login(api_key_value=API_KEY)
 
+    """A single feature group is feature constructed from data of the same source"""
+    # Reference: https://github.com/logicalclocks/hopsworks-tutorials/blob/master/batch-ai-systems/credit_scores/1_credit_scores_feature_backfill.ipynb
     fs: FeatureStore = project.get_feature_store(name="stock_predictor_featurestore")
+
+    # Feature groups will be offline since I dont need fast real-time predictions
 
     fg_feature: FeatureGroup = fs.get_or_create_feature_group(
         name="stock_features",
@@ -71,7 +93,7 @@ if __name__ == "__main__":
 
     fg_labels: FeatureGroup = fs.get_or_create_feature_group(
         name="stock_labels",
-        version=1,
+        version=1,  # TODO: try to auto increment versions and check if all data is overwritten
         description="stores stock prediction labels",
         primary_key=["date"],
         event_time="date",
@@ -79,3 +101,7 @@ if __name__ == "__main__":
     )
 
     fg_labels.insert(labels_df)
+
+
+if __name__ == "__main__":
+    main()
